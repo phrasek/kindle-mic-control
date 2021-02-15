7dfe1e354ded3f15b2a47e217da26786432435b5
Phrasek <64117215+phrasek@users.noreply.github.com>
2021-02-15 17:11:11 +0000

    first commit

---
+++ kindlemic.py
@@ -0,0 +1,94 @@
+import math
+import signal
+import struct
+import time
+
+import pyaudio
+from ahk import AHK
+from ahk.directives import NoTrayIcon
+
+Threshold = 15
+
+SHORT_NORMALIZE = 1.0 / 32768.0
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+swidth = 2
+
+debounce = 5
+
+kindle_path = r"path\\to\\kindle.exe"
+ahk_path = r"path\\to\\autohotkey.exe"
+
+
+class Listener:
+    @staticmethod
+    def rms(frame):
+        count = len(frame) / swidth
+        format = "%dh" % (count)
+        shorts = struct.unpack(format, frame)
+
+        sum_squares = 0.0
+        for sample in shorts:
+            n = sample * SHORT_NORMALIZE
+            sum_squares += n * n
+        rms = math.pow(sum_squares / count, 0.5)
+
+        return rms * 1000
+
+    def __init__(self, handler):
+        self.debug = False
+        self.p = pyaudio.PyAudio()
+        self.handler = handler
+        self.stream = self.p.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            output=True,
+            frames_per_buffer=chunk,
+        )
+
+    def listen(self):
+        lasttime = time.time() - 3
+
+        print("Listening...")
+        while True:
+            input = self.stream.read(chunk)
+            rms_val = self.rms(input)
+
+            if rms_val > Threshold and (lasttime - time.time()) < -2.5:
+                self.handler("right")
+                lasttime = time.time()
+            elif rms_val > Threshold and (lasttime - time.time()) < -0.25:
+                self.handler("left")
+                self.handler("left")
+                lasttime = time.time()
+            elif rms_val > Threshold:
+                pass
+
+
+def handler(key):
+    ahk = AHK(directives=[NoTrayIcon], executable_path=ahk_path)
+    win = ahk.find_window(process=kindle_path)
+    try:
+        win.activate()
+        win.to_top()
+        ahk.key_press(key)
+    except AttributeError:
+        pass
+
+
+def keyboardInterruptHandler(signal, frame):
+    exit()
+
+
+def main():
+    signal.signal(signal.SIGINT, keyboardInterruptHandler)
+    listener = Listener(handler=handler)
+    listener.listen()
+
+
+if __name__ == "__main__":
+    main()
